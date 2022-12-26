f71d9db24a26e250f7a102ed35da44f942018019
D-JSimpson <simpsondante03@gmail.com>
2022-12-26 16:54:00 -0500

    Refactor Code In Preparation For Modularity

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './style.css';
 import home from './home.js';
-import PizzaTake from "./PizzaTake.mp4";
-import PizzaTakeTwo from "./PizzaTakeTwo.mp4";
-import PizzaTakeThree from "./PizzaTakeThree.mp4";
+
 
 const body = document.querySelector("body");
 body.classList.add('myBG');
