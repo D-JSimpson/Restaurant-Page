4347cc1cc2ab62a788dfb93bb10acfe3cb1055a7
D-JSimpson <simpsondante03@gmail.com>
2022-12-26 17:25:52 -0500

    Get things ready to make menu module

--- src/home.js
+++ src/home.js
@@ -3,13 +3,6 @@ import PizzaTakeTwo from "./PizzaTakeTwo.mp4";
 import PizzaTakeThree from "./PizzaTakeThree.mp4";
 
 export default function home(){
-    window.onload = function(){ 
-        //now that the window has loaded we add our event listeners to the videos.
-        //When video1 has ended, play video2 etc
-        document.getElementById("pizzaTake").addEventListener("ended", function(){this.style.cssText = "z-index: 0"; playVideo("pizzaTakeTwo"); });
-        document.getElementById("pizzaTakeTwo").addEventListener("ended", function(){this.style.cssText = "z-index: 0"; playVideo("pizzaTakeThree"); });
-        document.getElementById("pizzaTakeThree").addEventListener("ended", function(){this.style.cssText = "z-index: 0"; playVideo("pizzaTake"); });
-    }
 
     function playVideo(videoID){
         //This playVideo function takes in the ID of a video element and plays that video.
@@ -35,20 +28,10 @@ export default function home(){
     storeSignText.appendChild(shop);
     storeSign.appendChild(storeSignText);
 
-    const navBar = document.createElement("ul");
-    let home = document.createElement("li");
-        home.innerText = "Home";
-    let menu = document.createElement("li");
-        menu.innerText = "Menu";
-    let contact = document.createElement("li");
-        contact.innerText = "Contact";  
-    navBar.appendChild(home); navBar.appendChild(menu); navBar.appendChild(contact);
-
-    content.appendChild(navBar);
-
     const videoContainer = document.createElement("div");
         videoContainer.id = "videoContainer";
     let pizzaTake = document.createElement("video");
+        pizzaTake.addEventListener("ended", function(){this.style.cssText = "z-index: 0"; playVideo("pizzaTakeTwo"); });
     pizzaTake.outerHTML
     pizzaTake.src = PizzaTake;
     pizzaTake.muted = true;
@@ -57,6 +40,7 @@ export default function home(){
     videoContainer.appendChild(pizzaTake);
 
     let pizzaTakeTwo = document.createElement("video");
+        pizzaTakeTwo.addEventListener("ended", function(){this.style.cssText = "z-index: 0"; playVideo("pizzaTakeThree"); });
     pizzaTakeTwo.outerHTML
     pizzaTakeTwo.src = PizzaTakeTwo;
     pizzaTakeTwo.muted = true;
@@ -65,6 +49,7 @@ export default function home(){
     videoContainer.appendChild(pizzaTakeTwo);
 
     let pizzaTakeThree = document.createElement("video");
+        pizzaTakeThree.addEventListener("ended", function(){this.style.cssText = "z-index: 0"; playVideo("pizzaTake"); });
     pizzaTakeThree.outerHTML
     pizzaTakeThree.src = PizzaTakeThree;
     pizzaTakeThree.muted = true;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import home from './home.js';
+import menu from './menu.js';
 
 
 const body = document.querySelector("body");
@@ -10,7 +11,25 @@ content.id = "content";
 
 body.appendChild(content);
 
+const navBar = document.createElement("ul");
+    let homeBtn = document.createElement("li");
+        homeBtn.innerText = "Home";
+        homeBtn.id = 'home';
+        homeBtn.addEventListener("click", () => {content.innerHTML = ""; home()});
+    let menuBtn = document.createElement("li");
+        menuBtn.innerText = "Menu";
+        menuBtn.id = 'menu';
+        menuBtn.addEventListener("click", () => {content.innerHTML = ""; menu()}); 
+    let contactBtn = document.createElement("li");
+        contactBtn.innerText = "Contact"; 
+        contactBtn.id = 'contact'; 
+    navBar.appendChild(homeBtn); navBar.appendChild(menuBtn); navBar.appendChild(contactBtn);
+
+    body.appendChild(navBar);
+
 home();
+
+          
 
 
 
