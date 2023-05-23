9b8c6e268d5002f97e7adb1076555843fb8d6ac7
D-JSimpson <simpsondante03@gmail.com>
2023-05-23 19:18:03 -0400

    Add contact cards with styling

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,6 @@
+import cheese from './imgs/Cheese.jpg';
+import wings from './imgs/chickenWing.jpg';
+import sprite from './imgs/sprite.jpeg';
 
 export default function contact(){
     
@@ -11,12 +14,48 @@ export default function contact(){
     let contactUs = document.createElement('p');
         contactUs.innerText = "CONTACT US";
 
+    const contactCardContainer = document.createElement('div');
+          contactCardContainer.id = "contactCardContainer";
+    
+    
+    
+    const contactCardCreation = (name, email, number, picture) => {
+        const sayHello = () =>console.log("hello!");
+        return {name, email, number, picture, sayHello};
+    };
+
+    let chiefP = contactCardCreation("Chief Pizza", "ChiefP@GPS.com", "555-2345", cheese);
+    let manW = contactCardCreation("Manager Wings", "manW@GPS.com", "457-2865", wings);
+    let deliS = contactCardCreation("Delivery Sprite", "deliS@GPS.com", "888-2758", sprite);
+    
+    const contactArray = [chiefP, manW, deliS];
+
+    contactArray.forEach(element =>{
+        let card = document.createElement('div');
+        card.classList.add("contactCard");
+        card.id = element.name.replace(" ", "");
+
+        
+        let name = document.createElement("p");
+            name.innerText = element.name;
+        let email = document.createElement("p");
+            email.innerText = element.email;
+        let number = document.createElement("p");
+            number.innerText = element.number;
+        let image = new Image();
+            image.src = element.picture;
+            image.classList.add("contactImg");
+        
+        card.appendChild(name);
+        card.appendChild(email);
+        card.appendChild(number);
+        card.appendChild(image);
+        contactCardContainer.appendChild(card);
+    });
+
     contactSignText.appendChild(contactUs);
     contactSign.appendChild(contactSignText);
     contactContainer.appendChild(contactSign);
+    contactContainer.appendChild(contactCardContainer)
     content.appendChild(contactContainer);
-    
-    const contactCard = (name, email, number, oicture) => {
-
-    }
 }
