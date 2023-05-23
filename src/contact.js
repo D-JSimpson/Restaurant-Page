66c12c47906905718e8bfdc2e218cb1115f14861
D-JSimpson <simpsondante03@gmail.com>
2023-05-23 17:25:55 -0400

    Create contact sign

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,22 @@
+
 export default function contact(){
     
+    const contactContainer = document.createElement('div');
+          contactContainer.id = "contactContainer";
+
+    const contactSign = document.createElement('div');
+          contactSign.id= "contactSign";
+    let contactSignText = document.createElement('div');
+        contactSignText.id = "contactSignText";
+    let contactUs = document.createElement('p');
+        contactUs.innerText = "CONTACT US";
+
+    contactSignText.appendChild(contactUs);
+    contactSign.appendChild(contactSignText);
+    contactContainer.appendChild(contactSign);
+    content.appendChild(contactContainer);
+    
+    const contactCard = (name, email, number, oicture) => {
+
+    }
 }
